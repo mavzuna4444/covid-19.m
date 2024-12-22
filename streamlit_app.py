2bcd34a959f914e5182cc6747120c662b3144607
mavzuna4444 <162799817+mavzuna4444@users.noreply.github.com>
2024-12-22 20:48:14 +0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,11 @@
 import streamlit as st
+import numpy as np
+import pandas as pd
+from sklearn.ensemble import RandomForestClassifier
+import matplotlib.pyplot as plt
+import seaborn as sns
+from sklearn.ensemble import BaggingClassifier
 
-st.title('🎈 App Name')
-
-st.write('Hello world!')
+st.title('Prediction of COVID-19')
+with st.expander('Data'):
+  data = pd.read_csv("covid-19 symptoms dataset.csv")
